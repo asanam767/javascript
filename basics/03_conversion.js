0d958ee63aa6fd08facd961451b39bf7450d4964
Katta Manasa <120806410+asanam767@users.noreply.github.com>
2024-07-11 14:35:22 +0000

    conversion

---
+++ basics/03_conversion.js
@@ -0,0 +1,28 @@
+a=null
+// console.log(a)
+let data=Number(a)
+// console.log(data)
+// if u try to convert a no, it will be easily converted.
+//if u try to convert a string to number- u get NaN: not a number
+// let boolvalue=true
+// let storevalue=Boolean(boolvalue)
+// let storeno=Number(boolvalue)
+// //  console.log(storevalue)
+// console.log(storeno)
+// a=1
+// console.log(a)
+// let bool=Boolean(a)
+// console.log(bool)
+// b="riya"
+// console.log(b)
+// let store=Boolean(b)
+// console.log(store)
+// 0-false
+// 1-true
+// ""-false
+// "riya"-true
+let n=33
+console.log(n)
+let store=String(n)
+console.log(store)
+console.log(typeof store)
